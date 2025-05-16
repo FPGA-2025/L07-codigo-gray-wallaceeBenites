35cfa4029d7a9734961e413bfc884d02f9aa3761
wallaceeBenites <wallacepuck@gmail.com>
2025-05-15 23:57:00 -0300

    teste

--- binary_to_gray.v
+++ binary_to_gray.v
@@ -3,6 +3,9 @@ module Binary_to_Gray (
     output wire [3:0] gray_out
 );
 
-// Insira seu codigo aqui
+assign gray_out[3] = binary_in[3];
+assign gray_out[2] = binary_in[3] ^ binary_in[2]; 
+assign gray_out[1] = binary_in[2] ^ binary_in[1];
+assign gray_out[0] = binary_in[1] ^ binary_in[0];
 
 endmodule
